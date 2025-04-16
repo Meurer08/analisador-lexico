48a3e97d0dcbf6eeb63b4d50f4c0bd6d8001e304
Vitor <meurervitor08@gmail.com>
2025-04-15 22:09:24 -0300

    feat: analisador lexico - class CompiladoresTR1

---
+++ src/CompiladoresTR1.java
@@ -0,0 +1,135 @@
+import java.io.*;
+import java.util.*;
+import java.util.regex.*;
+import java.util.stream.Collectors;
+
+public class CompiladoresTR1 {
+
+    enum TipoToken {//regez para captura
+        RESERVADA("^int|double|char|float|if|while|for", "reservada"),
+        IDENTIFICADOR("[A-Z][a-zA-Z]*", "identificador"),
+        NUMERO("(\\d+(,\\d+)?)", "número"),
+        OPERADOR("^<=|>=|==|=|<|>", "operador"),
+        SIMBOLO("[();]", "simbolo"),
+        WHITESPACE("^[ \t\r\n]+", "espaco em branco"),
+        COMENTARIO("^#.*", "comentario");
+
+        public final Pattern pattern;
+        public final String nome;
+
+        TipoToken(String regex, String nome) {
+            this.pattern = Pattern.compile(regex);
+            this.nome = nome;
+        }
+    }
+
+    static class Token {
+        TipoToken type;
+        String value;
+        int entrada = 0;
+
+        Token(TipoToken type, String value) {
+            this.type = type;
+            this.value = value;
+        }
+    }
+
+    public static List<Token> tokensExtraidosImput(String input) {
+
+        List<Token> tokens = new ArrayList<>();
+        var cleanInputList = input.replaceAll("#", "\n#")
+                .lines()
+                .filter(s -> !s.matches(TipoToken.COMENTARIO.pattern.toString()) && !s.isBlank())
+                .toList();
+
+
+        for (String s : cleanInputList) {
+
+            for (TipoToken type : TipoToken.values()) {
+                String regex = type.pattern.pattern();
+                Pattern pattern = Pattern.compile(regex);
+
+                String substring = s.trim();
+                Matcher matcher = pattern.matcher(substring);
+                switch (type) {
+                    case IDENTIFICADOR -> findTokens(type, matcher, tokens);
+                    case RESERVADA -> findTokens(type, matcher, tokens);
+                    case NUMERO -> findTokens(type, matcher, tokens);
+                    case OPERADOR -> findTokens(type, matcher, tokens);
+                    case SIMBOLO -> findTokens(type, matcher, tokens);
+                    case WHITESPACE -> findTokens(type, matcher, tokens);
+                    case COMENTARIO -> findTokens(type, matcher, tokens);
+                }
+            }
+        }
+
+        return tokens;
+    }
+
+    private static void findTokens(TipoToken type, Matcher matcher, List<Token> tokens) {
+        if (matcher.find()) {
+            String value = matcher.group();
+
+            if (type != TipoToken.WHITESPACE && tokens.stream().noneMatch(token -> token.type == type && Objects.equals(token.value, value))) {
+
+                tokens.add(new Token(type, value));
+            }
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sourceCode = new StringBuilder();
+        String line;
+        
+        System.out.println("Ctrl+D para enviar:");
+
+        while ((line = reader.readLine()) != null) {
+            sourceCode.append(line).append("\n");
+        }
+        System.out.println(sourceCode);
+        List<Token> tokens = tokensExtraidosImput(sourceCode.toString());
+
+
+        System.out.println("\nLista de Simbolos:");
+        int count = 1;
+        for (Token token : tokens) {
+
+           if (!("operador".equals(token.type.nome) || "simbolo".equals(token.type.nome))) {
+                token.entrada = count;
+            
+                System.out.println(new StringBuilder()
+                    .append(count++)
+                    .append(" | ")
+                    .append(token.value)
+                    .append(" | ")
+                    .append(token.type.nome));
+            
+                
+           }
+        }
+
+        String textoLimpo = sourceCode.toString().replaceAll("#", "\n#")
+                .lines()
+                .filter(s -> !s.matches(TipoToken.COMENTARIO.pattern.toString()) && !s.isBlank())
+                .collect(Collectors.joining());
+
+        for (Token token: tokens) {
+            String saidaToken = "<";
+
+            if (token.entrada == 0) {
+                saidaToken += token.value;
+                saidaToken += ",";
+            } else {
+                saidaToken += token.type.nome;
+                saidaToken += ",";
+                saidaToken += token.entrada;
+            }
+
+            saidaToken += "> ";
+
+            System.out.println(saidaToken);
+        }
+    }
+}
